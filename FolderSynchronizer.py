6b294ce84ddbe6a47a40bc2760a27aa7b5c4a92f
Katarina <katkahertelova@gmail.com>
2024-06-02 14:18:43 +0200

    set up logging to file and console

--- FolderSynchronizer.py
+++ FolderSynchronizer.py
@@ -1,7 +1,21 @@
+import logging
+
+
 class FolderSynchronizer:
     def __init__(self, source, replica, interval_seconds, log_file):
         self.source = source
         self.replica = replica
         self.interval_seconds = interval_seconds
         self.log_file = log_file
-        print(self.source, self.replica, self.log_file, self.interval_seconds)
+        self.setup_logging()
+
+    def setup_logging(self):
+        """
+        set up log format, log file name and logging to console
+        """
+        logging.basicConfig(level=logging.INFO,
+                            format='%(asctime)s - %(levelname)s - %(message)s',
+                            handlers=[
+                                logging.FileHandler(self.log_file),
+                                logging.StreamHandler()
+                            ])
