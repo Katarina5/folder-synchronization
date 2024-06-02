09ce5fafc5044e6b8471cb594605e66922e08bc2
Katarina <katkahertelova@gmail.com>
2024-06-02 14:07:35 +0200

    create FolderSynchronizer class

---
+++ FolderSynchronizer.py
@@ -0,0 +1,3 @@
+class FolderSynchronizer:
+    def __init__(self):
+        pass
---
+++ main.py
@@ -0,0 +1,11 @@
+from FolderSynchronizer import FolderSynchronizer
+
+
+def main():
+    """ Main program """
+    synchronizer = FolderSynchronizer()
+    return 0
+
+
+if __name__ == "__main__":
+    main()
