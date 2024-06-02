b815844a40177e72795d7b407ccd3ef881cd04cf
Katarina <katkahertelova@gmail.com>
2024-06-02 14:12:46 +0200

    add parser for directory paths, interval, and log file path

--- FolderSynchronizer.py
+++ FolderSynchronizer.py
@@ -1,3 +1,7 @@
 class FolderSynchronizer:
-    def __init__(self):
-        pass
+    def __init__(self, source, replica, interval_seconds, log_file):
+        self.source = source
+        self.replica = replica
+        self.interval_seconds = interval_seconds
+        self.log_file = log_file
+        print(self.source, self.replica, self.log_file, self.interval_seconds)
--- main.py
+++ main.py
@@ -1,9 +1,25 @@
+import argparse
 from FolderSynchronizer import FolderSynchronizer
+
+
+def get_arguments():
+    """
+    create argument parser that accepts command-line values
+    :return: ArgumentParser with parsed arguments
+    """
+    parser = argparse.ArgumentParser(prog='Folder Synchronization')
+    parser.add_argument('--source_folder', help='Path of the source folder', type=str)
+    parser.add_argument('--replica_folder', help='Path of the replica folder', type=str)
+    parser.add_argument('--interval_seconds', help='Interval of synchronization (in seconds)', type=int)
+    parser.add_argument('--log_file', help='Path to the log file', type=str)
+
+    return parser.parse_args()
 
 
 def main():
     """ Main program """
-    synchronizer = FolderSynchronizer()
+    args = get_arguments()
+    synchronizer = FolderSynchronizer(args.source_folder, args.replica_folder, args.interval_seconds, args.log_file)
     return 0
 
 
